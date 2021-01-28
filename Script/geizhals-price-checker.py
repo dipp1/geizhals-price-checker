d5b6e97b82490baf350bf56d439911138bb259de
Tiberiu-Arthur Nowotny <thundersocks69@gmail.com>
2021-01-28 23:12:13 +0100

    First test script with comments and hello world

---
+++ Script/geizhals-price-checker.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''
+Authors: Tiberiu-Arthur Nowotny, Buraczewska Diana
+Purpose of this script: Reads geizhals links and checks for required prices periodically
+Last updaten on:
+'''
+
+'''TODO DIANA
+Create file configuration
+Read file and create a list / array of links
+Implement save config method
+
+config example
+
+[Geizhals Links]
+geizhals.at/....
+geizhals.at/....
+geizhals.at/....
+etc.
+'''
+
+
+
+'''TODO Arthur
+
+Create method to iterate over list /array
+Get html site and search for matching price in geizhals table
+
+'''
+print("hello world")
