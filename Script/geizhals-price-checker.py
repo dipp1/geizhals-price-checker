994563357a620124dc49ad9519a6cf4cfd00c0cc
Tiberiu-Arthur Nowotny <thundersocks69@gmail.com>
2021-01-29 13:19:56 +0100

    Loading of geizhals links now works thanks to User Agent

--- Script/geizhals-price-checker.py
+++ Script/geizhals-price-checker.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+
+
+
+
+
 '''
 Authors: Tiberiu-Arthur Nowotny, Buraczewska Diana
 Purpose of this script: Reads geizhals links and checks for required prices periodically
@@ -27,4 +32,19 @@ Create method to iterate over list /array
 Get html site and search for matching price in geizhals table
 
 '''
-print("hello world")
+link = 'https://geizhals.at/?cat=monlcd19wide&xf=11939_23~11955_IPS~11963_144~14591_19201080&asuch=&bpmin=&bpmax=&v=e&hloc=at&plz=&dist=&mail=&sort=p&bl1_id=30#productlist'
+
+
+# from urllib import request
+#
+# with request.urlopen(link, headers={'User-Agent': 'Mozilla/5.0'}) as response:
+#     data = response.read()
+#
+
+from urllib.request import Request, urlopen
+
+req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
+webpage = urlopen(req).read()
+
+
+print(webpage.decode('utf8'))
