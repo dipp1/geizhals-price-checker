0d7118e04991de72e1996faf653f3700664d88ef
Tiberiu-Arthur Nowotny <thundersocks69@gmail.com>
2021-02-01 20:36:52 +0100

    Changed sleep time for 1 hour

--- Script/geizhals-price-checker.py
+++ Script/geizhals-price-checker.py
@@ -65,12 +65,13 @@ def get_webSite(link):  # returns a HTML page which can be read by the xPath
 
 
 def main():
+
     aList = []
     aList.append(Product(200, "https://geizhals.at/?cat=monlcd19wide&xf=11939_23~11955_IPS~11963_144~14591_19201080&asuch=&bpmin=&bpmax=&v=e&hloc=at&plz=&dist=&mail=&sort=p&bl1_id=30#productlist"))
     aList.append(Product(600.50, "https://geizhals.at/?cat=monlcd19wide&v=e&hloc=at&sort=p&bl1_id=30&xf=11939_23%7E11955_IPS%7E11963_240%7E14591_19201080"))
     aList.append(Product(700.50, "https://geizhals.at/?cat=cpuamdam4&xf=25_6%7E5_PCIe+4.0%7E5_SMT%7E820_AM4"))
     # aList.append(Product(700.50, "https://geizhals.at/?cat=cpuamdam4&xf=25_6%7E5_PCIe+4.0%7E5_SMT%7E820_AM4"))
-    aList.append(Product(150.50, "https://geizhals.at/"))
+    # aList.append(Product(150.50, "https://geizhals.at/"))
     #Liste an Produkten wurde generiert
     checkForProduct(aList)
 
@@ -105,7 +106,7 @@ def checkForProduct(aList):
 
                 if (produktListObject.askPrice >= price): # If a product with a good price exists, then send me E-mail
                     print(f"PREISALARM: {name} ist gerade für {price}€ zu haben!")
-                    # sendMail(name, price, link) # Calls function to send email WORKS
+                    sendMail(name, price, link) # Calls function to send email WORKS
                     time.sleep(3)
 
 
@@ -126,9 +127,9 @@ def checkForProduct(aList):
             i += 1 # auti increment of our counter variable for our for loop
         i = 0 # Sets our i for the "for loop" to 0 after being done with the loop, so that it can restart when the while loop restarts
 
-        # print("Sleep for one hour")
-        # time.sleep(3600) # stops the script and waits one hour before polling again
-        time.sleep(8)
+        print("Sleep for one hour")
+        time.sleep(3600) # stops the script and waits one hour before polling again
+        # time.sleep(8)
 
 
 
@@ -139,12 +140,12 @@ def checkForProduct(aList):
 
 
 def sendMail(productName, productPrice, productLink):
-    sender = 'senderEmail'
-    recipient = 'empfängerEmail'
+    sender = 'SenderEmail'
+    recipient = 'EmpfängerEmail'
     smtp_user = sender
-    smtp_pass = "Passwort"
-    smtp_host = "mail.gmx.net"
-    smtp_port = "587"
+    smtp_pass = "SenderPasswort"
+    smtp_host = "SMTP Host"
+    smtp_port = "SMTP Port"
 
     msg = EmailMessage()
     message_string = f"PREISALARM: {productName} ist gerade für {productPrice}€ zu haben!\n"
@@ -158,6 +159,7 @@ def sendMail(productName, productPrice, productLink):
             smtp.starttls()
             smtp.login(smtp_user,smtp_pass)
             smtp.send_message(msg)
+            print("Email gesendet")
     except Exception as e:
         print("Problem beim E-Mail versenden")
         print(e)
