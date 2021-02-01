f9186f569b97707d77465f97f93f0ae3a531a390
Diana Buraczewska <diana.buraczewska@gmail.com>
2021-02-01 20:14:36 +0100

    Minor changes

--- geizhals-price-checker.py
+++ geizhals-price-checker.py
@@ -1,32 +1,15 @@
 #!/usr/bin/env python3
-'''
-Authors: Tiberiu-Arthur Nowotny, Buraczewska Diana
-Purpose of this script: Reads geizhals links and checks for required prices periodically
-Last updaten on:
-'''
+"""
+Authors:                    Tiberiu-Arthur Nowotny, Buraczewska Diana
+Purpose of this script:     Reads Geizhals links and checks for required prices periodically
+Last updated on:            01.02.2021
+"""
+
 from urllib.request import Request, urlopen
 from lxml import html
 
-class Product: # Class Product which needs a price and link, represents a product
-    price = 0.0
-    name = ""
-    link = ""
-    pictureLink = ""
-
-    def __init__(self, askPrice, link):
-        self.link = link
-        self.askPrice = askPrice
-
-    def get_webSitee(self):
-        req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-        return  urlopen(req).read()
-
-
-    def changeName(self):
-        self.name = "Lepin :D"
-
-
-'''TODO DIANA
+# TODO DIANA
+"""
 Create file configuration
 Read file and create a list / array of links
 Implement save config method
@@ -38,34 +21,28 @@ config example
 312.50,geizhals.at/....
 250, geizhals.at/....
 etc.
-'''
+"""
 
-
-
-'''TODO Arthur
-
+# TODO Arthur
+"""
 Create method to iterate over list /array
 Get html site and search for matching price in geizhals table
+"""
 
-'''
-link = 'https://geizhals.at/?cat=monlcd19wide&xf=11939_23~11955_IPS~11963_144~14591_19201080&asuch=&bpmin=&bpmax=&v=e&hloc=at&plz=&dist=&mail=&sort=p&bl1_id=30#productlist'
-# link = 'https://geizhals.at/?cat=monlcd19wide&v=e&hloc=at&sort=p&bl1_id=30&xf=11939_23%7E11955_IPS%7E11963_240%7E14591_19201080'
-# link = 'https://geizhals.at/?cat=cpuamdam4&xf=25_6%7E5_PCIe+4.0%7E5_SMT%7E820_AM4'
-link = "https://geizhals.eu/?cat=sysdiv&xf=3126_1920x1080~8419_sonstige&asuch=&bpmin=&bpmax=&v=e&hloc=at&hloc=de&hloc=pl&hloc=uk&hloc=eu&plz=&dist=&mail=&sort=p&bl1_id=30#productlist"
+# link = 'https://geizhals.at/?cat=monlcd19wide&xf=11939_23~11955_IPS~11963_144~14591_19201080&asuch=&bpmin=&bpmax=&v
+# =e' \ '&hloc=at&plz=&dist=&mail=&sort=p&bl1_id=30#productlist ' link =
+# 'https://geizhals.at/?cat=monlcd19wide&v=e&hloc=at&sort=p&bl1_id=30&xf=11939_23%7E11955_IPS%7E11963_240
+# %7E14591_19201080' link = 'https://geizhals.at/?cat=cpuamdam4&xf=25_6%7E5_PCIe+4.0%7E5_SMT%7E820_AM4'
+link = "https://geizhals.eu/?cat=sysdiv&xf=3126_1920x1080~8419_sonstige&asuch=&bpmin=&bpmax=&v=e&hloc=at&hloc=de&hloc" \
+       "=pl&hloc=uk&hloc=eu&plz=&dist=&mail=&sort=p&bl1_id=30#productlist "
 
 
 def get_webSite():
     req = Request(link, headers={'User-Agent': 'Mozilla/5.0'})
-    return  urlopen(req).read()
+    return urlopen(req).read()
 
-
-
-
-webpage = get_webSite() # Contains all HTML from the site
+webpage = get_webSite()  # Contains all HTML from the site
 root = html.fromstring(webpage)
-
-
-
 
 price = root.xpath("//*[@id=\"product0\"]/div[6]/span/span")[0].text.strip()
 name = root.xpath("//*[@id=\"product0\"]/div[2]/a/span")[0].text.strip()
@@ -76,23 +53,19 @@ picture = root.xpath("//*[@id=\"product0\"]/div[1]/a/div/picture/source/@srcset"
 # the @ refers to the attribute of the selected element, / slashes seem to separate the searched terms
 # The [0] refers to the first element of a list, we use this because xPath returns a list with exactly one item
 
-price = price.lstrip('€ ') # removes the euro sign and the space
-price = price.replace(',', '.') # removes the comma with a dot
-price = float(price) # converts price string to float
+price = price.lstrip('€ ')  # removes the euro sign and the space
+price = price.replace(',', '.')  # removes the comma with a dot
+price = float(price)  # converts price string to float
 price = price + 30
 
 print(f"Price : {price}")
-print("Name : " + (name))
-print("Link : " + (link))
+print("Name : " + name)
+print("Link : " + link)
 print("PictureLink : " + (picture.split(" ")[2]))
 
+a_list = ["Kater", "Mauser", "Hunder"]
 
-aList = []
-aList.append("Kater")
-aList.append("Mauser")
-aList.append("Hunder")
-
-for animal in aList:
+for animal in a_list:
     print(animal)
 
 
