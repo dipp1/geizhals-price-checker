808a5d23486fb131278dbf2950636db38b0c6dbf
Diana Buraczewska <diana.buraczewska@gmail.com>
2021-02-02 22:38:55 +0100

    Add argparser for email config and print statement for error messages if not running on windows

--- geizhals-price-checker.py
+++ geizhals-price-checker.py
@@ -10,7 +10,9 @@ import platform
 import re
 import time
 import urllib.error
-from win10toast import ToastNotifier
+
+if platform.system() == 'Windows':
+    from win10toast import ToastNotifier  # noqa
 
 from geizhals_price_checker.messenger import Messenger
 from geizhals_price_checker.product import Product
@@ -31,7 +33,6 @@ def read_products_from_file(path):
 
 
 def read_config_from_file(path):
-    # sender, smtp_user, smtp_pass, smtp_host, smtp_port
     c = {}
     with open(path, 'r') as f:
         for line in f:
@@ -44,8 +45,7 @@ def read_config_from_file(path):
     return c
 
 
-def check_for_product(products, messenger, recipient='max.nowotny.512@gmail.com'):
-
+def check_for_product(products, messenger, recipient):
     error = False
 
     while error is False:  # Executes the code as long as no heavy errors occur
@@ -57,7 +57,7 @@ def check_for_product(products, messenger, recipient='max.nowotny.512@gmail.com'
                 price = website.xpath("//*[@id=\"product0\"]/div[6]/span/span")[0].text.strip()
                 name = website.xpath("//*[@id=\"product0\"]/div[2]/a/span")[0].text.strip()
                 link = "https://geizhals.at/" + website.xpath("//*[@id=\"product0\"]/div[2]/a/@href")[0]
-                picture = website.xpath("//*[@id=\"product0\"]/div[1]/a/div/picture/source/@srcset")[0]
+                picture = website.xpath("//*[@id=\"product0\"]/div[1]/a/div/picture/source/@srcset")[0]  # noqa
 
                 price = price.lstrip('€ ')  # removes the euro sign and the space
                 price = price.replace(',', '.')  # removes the comma with a dot
@@ -70,21 +70,52 @@ def check_for_product(products, messenger, recipient='max.nowotny.512@gmail.com'
                     # print("Mail sent ;)")
                     time.sleep(3)
 
-            except urllib.error.URLError: # When Internet is down or site returns a 404 this exception is triggered and stops the script
+            except urllib.error.URLError:
+                # When Internet is down or site returns a 404 this exception is triggered and stops the script
                 error = True
-                toaster = ToastNotifier()
-                toaster.show_toast("Geizhals-price-checker error", f"Netzwerkfehler: Konnte keine Informationen aus dem Internet Laden, oder Seite returnt 404 Status! Script beendet.", duration=10, threaded=True)
+                if platform.system() == 'Windows':
+                    toaster = ToastNotifier()
+                    toaster.show_toast("Geizhals-price-checker error",
+                                       f"Netzwerkfehler: Konnte keine Informationen aus "
+                                       f"dem Internet Laden, oder Seite returns 404 "
+                                       f"Status! Script beendet.", duration=10,
+                                       threaded=True)
+                else:
+                    print(f"Netzwerkfehler: Konnte keine Informationen aus "
+                          f"dem Internet Laden, oder Seite returns 404 "
+                          f"Status! Script beendet.")
                 break
-            except IndexError: #When a wrong link is being read or a category has 0 products, this exception will be triggered
-                toaster = ToastNotifier()
-                toaster.show_toast("Geizhals-price-checker error", f"Seiten Ladefehler: Konnte keine Informationen für Link {product.url} aus dem Internet Laden! Überprüfe die Links!", duration=240, threaded=True)
-        if (error == False):
+            except IndexError:
+                # When a wrong link is being read or a category has 0 products, this exception will be triggered
+                if platform.system() == 'Windows':
+                    toaster = ToastNotifier()
+                    toaster.show_toast("Geizhals-price-checker error",
+                                       f"Seiten Ladefehler: Konnte keine Informationen "
+                                       f"für Link {product.url} aus dem Internet Laden! "
+                                       f"Überprüfe die Links!", duration=240,
+                                       threaded=True)
+                else:
+                    print(f"Seiten Ladefehler: Konnte keine Informationen "
+                          f"für Link {product.url} aus dem Internet Laden! "
+                          f"Überprüfe die Links!")
+        if not error:
             print("Sleep for one hour")
             time.sleep(3600)  # stops the script and waits one hour before polling again
-        # else:
-        #     print("Error and script stops")
+
 
 def main():
+    import argparse
+
+    # Parser to read email configuration from command line. run python geizhals-price-checker.py --help to get help.
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--sender', type=str, required=True, help='E-Mail address of sender.')
+    parser.add_argument('--recipient', type=str, required=True, help='E-Mail address of recipient.')
+    parser.add_argument('--smtp_pass', type=str, required=True, help='Password of sender E-Mail.')
+    parser.add_argument('--smtp_host', default='mail.gmx.net', type=str, help='Sender E-Mail host (default: '
+                                                                              'mail.gmx.net).')
+    parser.add_argument('--smtp_port', default=587, type=int, help='SMTP port to use (default: 587).')
+    args = parser.parse_args()
+
     data = read_products_from_file('price_list.txt')
     target_prices = data['target_prices']
     urls = data['urls']
@@ -93,11 +124,12 @@ def main():
     for target_price, url in zip(target_prices, urls):
         products.append(Product(target_price, url))
 
-    config = read_config_from_file('configuration.txt')
-
+    config = {'sender': args.sender, 'recipient': args.recipient,
+              'smtp_pass': args.smtp_pass, 'smtp_host': args.smtp_host, 'smtp_port': args.smtp_port}
+    print(config)
     messenger = Messenger(config)
 
-    check_for_product(products, messenger)
+    check_for_product(products, messenger, recipient=config['recipient'])
 
 
 if __name__ == '__main__':
